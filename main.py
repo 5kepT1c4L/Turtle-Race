fbce4622b6a50ca08109a3dd62b27342e853bb2e
Eric Jing <70067413+5kepT1c4L@users.noreply.github.com>
2021-08-30 11:05:59 -0400

    Backend and front end aspect of tkinter module finished

--- main.py
+++ main.py
@@ -1,2 +1,62 @@
-import random
 import turtle
+from tkinter import *
+import random
+
+Bank_account = 0
+
+
+## Turtle Game Setup
+def turtle_stuff():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def enter_input():
+    global turtle_bet
+    global bet_amount
+
+    turtle_bet = user_turtle_bet.get()
+    bet_amount = user_bet_amount.get()
+    turtle_stuff() # trigger the turtle code
+    return turtle_bet, bet_amount
+
+
+## Tkinter Entry Setup
+tk = Tk()
+
+user_turtle_bet = StringVar()
+user_bet_amount = StringVar()
+
+user_turtle_label = Label(tk, text="Turtle Bet", font=('Times', 13, 'bold'))
+user_turtle_entry = Entry(tk, textvariable=user_turtle_bet, font=('Times', 13, 'normal'))
+
+user_amount_label = Label(tk, text="Bet Amount", font=('Times', 13, 'bold'))
+user_amount_entry = Entry(tk, textvariable=user_bet_amount, font=('Times', 13, 'normal'))
+
+enter_button = Button(tk, text="ENTER", command=enter_input)
+
+user_turtle_label.grid(row=0, column=0)
+user_turtle_entry.grid(row=0, column=1)
+user_amount_label.grid(row=1, column=0)
+user_amount_entry.grid(row=1, column=1)
+enter_button.grid(row=2, column=1)
+
+tk.mainloop()
+
+
+
+
+
+
+
+
